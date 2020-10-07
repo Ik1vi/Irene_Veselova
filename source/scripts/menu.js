486cbe98c8cd64e58c22c78069fed18ca027565a
Irene Veselova <ik1vikuz@gmail.com>
2020-10-07 16:14:23 +0300

    fixes pixel perfect

--- source/scripts/menu.js
+++ source/scripts/menu.js
@@ -7,9 +7,15 @@ btnMenuEl.addEventListener('click', function(ev) {
 			headerEl.classList.remove('menu-open');
 			bodyEl.classList.remove('js-fixed');
 		});
-	}
+    }
+    
+    if(headerEl.classList.contains('menu-open') ) {
+        btnMenuEl.setAttribute('aria-label', 'Скрыть меню');
+    } else {
+        btnMenuEl.setAttribute('aria-label', 'пПоказать меню');
+    }
 
-	if ( (window.matchMedia('(max-width: 1023px)').matches) && (headerEl.classList.contains('menu-open')) ) {
+	if( (window.matchMedia('(max-width: 1023px)').matches) && (headerEl.classList.contains('menu-open')) ) {
 		bodyEl.classList.add('js-fixed');
 	} else {
 		bodyEl.classList.remove('js-fixed');
