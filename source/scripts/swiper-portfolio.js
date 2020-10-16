80718c440b8c838e8185c701ffeafe0e774cd897
Irene Veselova <ik1vikuz@gmail.com>
2020-10-16 18:04:57 +0300

    swiper response & socials

--- source/scripts/swiper-portfolio.js
+++ source/scripts/swiper-portfolio.js
@@ -1,20 +1,43 @@
 const slider = document.querySelector('.swiper-container');
 
 let mySwiper = new Swiper(slider, {
-    slidesPerView: 3,
-    speed: 400,
-    spaceBetween: 35,
-    loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  loop: true,
 
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-
-    pagination: {
+  pagination: {
     el: '.swiper-pagination',
     type: 'bullets',
     clickable: true,
+  },
+
+  navigation: {
+    hiddenClass: 'swiper-button-hidden',
+  },
+  
+  breakpoints: {
+    570: {
+      slidesPerView: 2,
+      spaceBetween: 35,
     },
 
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+      navigation: {
+        hiddenClass: 'swiper-button-hidden',
+      },
+    },
+
+    1170: {
+      slidesPerView: 3,
+      speed: 300,
+      spaceBetween: 35,
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    }
+  }
 });
